2d0d7163f1a850a5c627fb81ef02e12c44c5204a
Oleh <ivaniukoleh2@gmail.com>
2024-10-13 11:59:33 +0300

    add yml

--- src/win/UART_Client_Lab_2/UART_Client_Lab_2.cpp
+++ src/win/UART_Client_Lab_2/UART_Client_Lab_2.cpp
@@ -4,19 +4,37 @@
 
 using namespace std;
 
+
+string createXML(const string& message) {
+    return "<message>" + message + "</message>";
+}
+
+string parseXML(const string& xml) {
+    size_t start = xml.find("<message>") + 9; // 9 - це довжина тегу <message>
+    size_t end = xml.find("</message>");
+
+    if (start != string::npos && end != string::npos) {
+        return xml.substr(start, end - start);
+    }
+
+    return ""; // Якщо XML некоректний
+}
+
+
+
 int main() {
     // Запит номера COM-порту у користувача
-    int portNumber;
-    cout << "Enter COM port number (e.g., 6 for COM6): ";
-    cin >> portNumber;
+    //int portNumber;
+    //cout << "Enter COM port number (e.g., 6 for COM6): ";
+    //cin >> portNumber;
 
-    // Формування назви порту
-    string portName = "COM" + to_string(portNumber);
+    //// Формування назви порту
+    //string portName = "COM" + to_string(portNumber);
 
-    //const char* portName = "COM6";
+    const char* portName = "COM6";
 
     // Відкриття COM-порту
-    HANDLE hSerial = CreateFileA(portName.c_str(),
+    HANDLE hSerial = CreateFileA(portName, // portName.c_str()
         GENERIC_READ | GENERIC_WRITE,
         0,
         NULL,
@@ -59,17 +77,22 @@ int main() {
         return 1;
     }
 
+    Sleep(2000);
+
     // Цикл для безкінечної відправки повідомлень
     while (true) {
         string message = "hello from pc";  // Повідомлення для відправки
+        string messageXML = createXML(message);
         DWORD bytesWritten;
 
+        
+
         // Відправка повідомлення до Arduino
-        if (!WriteFile(hSerial, message.c_str(), message.size(), &bytesWritten, NULL)) {
+        if (!WriteFile(hSerial, messageXML.c_str(), messageXML.size(), &bytesWritten, NULL)) {
             cerr << "Error writing to port" << endl;
             break;
         }
-        cout << "Sent: " << message << endl;
+        cout << "Sent: " << message << " (" << messageXML << ")" << endl;
 
         // Читання відповіді від Arduino
         char buffer[128];
@@ -78,7 +101,7 @@ int main() {
         if (ReadFile(hSerial, buffer, sizeof(buffer) - 1, &bytesRead, NULL)) {
             buffer[bytesRead] = '\0';
             if (bytesRead > 0) {
-                cout << "Received from Arduino: " << buffer << endl;
+                cout << "Received from Arduino: " << parseXML(buffer) << " (" << buffer << ")" << endl << endl;
             }
         }
         else {
