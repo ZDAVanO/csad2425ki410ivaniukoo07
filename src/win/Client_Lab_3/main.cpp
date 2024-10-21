d51e714596000db044652c07cb2ed6da69d3a586
Oleh <ivaniukoleh2@gmail.com>
2024-10-22 00:14:20 +0300

    title bar icon, reports

--- src/win/Client_Lab_3/main.cpp
+++ src/win/Client_Lab_3/main.cpp
@@ -7,6 +7,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.setWindowTitle("TicTacToe");
+    QIcon icon(":/icons/ttt_icon.ico");
+    w.setWindowIcon(icon);
     w.show();
     return a.exec();
 }
